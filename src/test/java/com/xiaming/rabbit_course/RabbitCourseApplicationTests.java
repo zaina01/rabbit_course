f6d0d490b3c60ab572b70b23387b519259b9aa07
ming <2623338082@qqq.com>
2023-03-09 15:38:33 +0800

    创建项目

---
+++ src/test/java/com/xiaming/rabbit_course/RabbitCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaming.rabbit_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
